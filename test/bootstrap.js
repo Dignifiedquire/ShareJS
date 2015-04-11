17b28b9c971aac310835a945109ef37be578b2e5
dignifiedquire <dignifiedquire@gmail.com>
2015-04-11 15:47:17 +0200

    client(doc): Start testing.

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -2,5 +2,8 @@
 // ==============
 
 import chai from 'chai'
+import sinon from 'sinon'
 
+chai.use(require('sinon-chai'))
 global.expect = chai.expect
+global.sinon = sinon
--- test/client/doc.spec.js
+++ test/client/doc.spec.js
@@ -1,7 +1,55 @@
+import {EventEmitter} from 'events'
+
 import Doc from '../../lib/client/doc'
 
 describe('doc', () => {
-  it('exists', () => {
-    expect(Doc).to.be.a.function
+  let connection
+
+  beforeEach(() => {
+    connection = {
+      _destroyDoc: sinon.stub()
+    }
+  })
+
+  describe('constructor', () => {
+    it('inherits from EventEmitter', () => {
+      const doc = new Doc({}, 'people', 'alice')
+
+      expect(doc).to.be.an.instanceOf(EventEmitter)
+    })
+
+    it('assigns the properties', () => {
+      const doc = new Doc(connection, 'people', 'alice')
+
+      expect(doc).to.have.property('connection', connection)
+      expect(doc).to.have.property('collection', 'people')
+      expect(doc).to.have.property('name', 'alice')
+    })
+  })
+
+  describe('destroy', () => {
+    let doc
+    beforeEach(() => {
+      doc = new Doc(connection, 'people', 'alice')
+    })
+
+    afterEach(() => {
+      connection._destroyDoc.reset()
+    })
+
+    it('destroys the remote doc', done => {
+      doc.destroy(() => {
+        expect(connection._destroyDoc).to.have.been.calledWith(doc)
+        done()
+      })
+    })
+
+    it('removes all contexts', done => {
+      sinon.spy(doc, 'removeContexts')
+      doc.destroy(() => {
+        expect(doc.removeContexts).to.have.been.calledOnce
+        done()
+      })
+    })
   })
 })
