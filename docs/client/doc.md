2dabdb2175484e3afc184167480c3e3d675954d5
dignifiedquire <dignifiedquire@gmail.com>
2015-04-11 18:47:11 +0200

    Start docs and extract context creation.

---
+++ docs/client/doc.md
@@ -0,0 +1,39 @@
+# Doc
+
+## `constructor`
+
+## `ingestData`
+
+## `getSnapshot`
+
+## `whenReady`
+
+## `retry`
+
+## `hasPending`
+
+## `flush`
+
+## `subscribe`
+
+## `unsubscribe`
+
+## `fetch`
+
+## `submitOp`
+
+## `create`
+
+## `del`
+
+## `createContext`
+
+Returns a [`Context`](context.md).
+
+## `pause`
+
+## `resume`
+
+## `destroy`
+
+## `removeContexts`
--- test/client/doc.spec.js
+++ test/client/doc.spec.js
@@ -4,52 +4,132 @@ import Doc from '../../lib/client/doc'
 
 describe('doc', () => {
   let connection
-
-  beforeEach(() => {
-    connection = {
-      _destroyDoc: sinon.stub()
-    }
-  })
-
-  describe('constructor', () => {
-    it('inherits from EventEmitter', () => {
-      const doc = new Doc({}, 'people', 'alice')
-
-      expect(doc).to.be.an.instanceOf(EventEmitter)
-    })
-
-    it('assigns the properties', () => {
-      const doc = new Doc(connection, 'people', 'alice')
-
-      expect(doc).to.have.property('connection', connection)
-      expect(doc).to.have.property('collection', 'people')
-      expect(doc).to.have.property('name', 'alice')
-    })
-  })
-
-  describe('destroy', () => {
-    let doc
+  describe('public api', () => {
     beforeEach(() => {
-      doc = new Doc(connection, 'people', 'alice')
+      connection = {
+        _destroyDoc: sinon.stub()
+      }
     })
 
-    afterEach(() => {
-      connection._destroyDoc.reset()
-    })
+    describe('constructor', () => {
+      it('inherits from EventEmitter', () => {
+        const doc = new Doc({}, 'people', 'alice')
 
-    it('destroys the remote doc', done => {
-      doc.destroy(() => {
-        expect(connection._destroyDoc).to.have.been.calledWith(doc)
-        done()
+        expect(doc).to.be.an.instanceOf(EventEmitter)
+      })
+
+      it('assigns the properties', () => {
+        const doc = new Doc(connection, 'people', 'alice')
+
+        expect(doc).to.have.property('connection', connection)
+        expect(doc).to.have.property('collection', 'people')
+        expect(doc).to.have.property('name', 'alice')
       })
     })
 
-    it('removes all contexts', done => {
-      sinon.spy(doc, 'removeContexts')
-      doc.destroy(() => {
-        expect(doc.removeContexts).to.have.been.calledOnce
-        done()
+    describe('ingestData', () => {
+
+    })
+
+    describe('getSnapshot', () => {
+
+    })
+
+    describe('whenReady', () => {
+
+    })
+
+    describe('retry', () => {
+
+    })
+
+    describe('hasPending', () => {
+
+    })
+
+    describe('flush', () => {
+
+    })
+
+    describe('subscribe', () => {
+
+    })
+
+    describe('unsubscribe', () => {
+
+    })
+
+    describe('fetch', () => {
+
+    })
+
+    describe('submitOp', () => {
+
+    })
+
+    describe('create', () => {
+
+    })
+
+    describe('del', () => {
+
+    })
+
+    describe('createContext', () => {
+
+    })
+
+    describe('pause', () => {
+
+    })
+
+    describe('resume', () => {
+
+    })
+
+
+
+    describe('destroy', () => {
+      let doc
+      beforeEach(() => {
+        doc = new Doc(connection, 'people', 'alice')
       })
+
+      afterEach(() => {
+        connection._destroyDoc.reset()
+      })
+
+      it('destroys the remote doc', done => {
+        doc.destroy(() => {
+          expect(connection._destroyDoc).to.have.been.calledWith(doc)
+          done()
+        })
+      })
+
+      it('removes all contexts', done => {
+        sinon.spy(doc, 'removeContexts')
+        doc.destroy(() => {
+          expect(doc.removeContexts).to.have.been.calledOnce
+          done()
+        })
+      })
+
+      it('waits for nothing pending to destroy the doc', done  => {
+        doc.action = 'subscribe'
+        sinon.spy(doc, 'emit')
+
+        doc._clearAction()
+
+        doc.destroy(() => {
+          expect(doc.emit).to.have.been.calledBefore(connection._destroyDoc)
+          expect(doc.hasPending()).to.be.eql(false)
+          done()
+        })
+      })
+    })
+
+    describe('removeContexts', () => {
+
     })
   })
 })
