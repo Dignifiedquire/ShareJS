5d024db0d51e500aac619367b3d4630448e20d8b
dignifiedquire <dignifiedquire@gmail.com>
2015-04-12 10:46:44 +0200

    client(doc): More tests and cleanup.

--- test/client/doc.spec.js
+++ test/client/doc.spec.js
@@ -27,12 +27,70 @@ describe('doc', () => {
       })
     })
 
-    describe('ingestData', () => {
+    describe('ingestData', done => {
+      let doc
 
+      beforeEach(() => {
+        doc = new Doc(connection, 'people', 'alice')
+      })
+
+      it('applies the data to the doc', () => {
+        const ready = sinon.stub()
+        doc.on('ready', ready)
+
+        doc.ingestData({
+          v: 12,
+          data: 'hello world',
+          type: 'text'
+        })
+
+        expect(doc).to.have.property('version', 12)
+        expect(doc).to.have.property('snapshot', 'hello world')
+        expect(doc).to.have.property('state', 'ready')
+        expect(ready).to.have.been.calledOnce
+      })
+
+      it('throws an error when missing the version', () => {
+        expect(() => {
+          doc.ingestData({})
+        }).to.throw('Missing version in ingested data people alice.')
+      })
+
+      it('ignores the data if the state is already set', () => {
+        doc.state = 'floating'
+
+        doc.ingestData({
+          v: 12,
+          data: 'hello world',
+          type: 'text'
+        })
+
+        expect(doc).to.have.property('version', null)
+        expect(doc).to.have.property('snapshot', undefined)
+        expect(doc).to.have.property('state', 'floating')
+      })
+
+      it('ignores newer versions', () => {
+        doc.version = 13
+        doc.state = 'ready'
+
+        doc.ingestData({
+          v: 12,
+          data: 'hello world',
+          type: 'text'
+        })
+
+        expect(doc).to.have.property('version', 13)
+      })
     })
 
     describe('getSnapshot', () => {
+      it('returns the current snapshot', () => {
+        const doc = new Doc(connection, 'people', 'alice')
+        doc.snapshot = 'hello world'
 
+        expect(doc.getSnapshot()).to.be.eql('hello world')
+      })
     })
 
     describe('whenReady', () => {
