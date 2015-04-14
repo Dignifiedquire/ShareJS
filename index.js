1f226d7b00856b6d8658e3e7a4c0fc8ee0812951
dignifiedquire <dignifiedquire@gmail.com>
2015-04-15 00:40:34 +0200

    Start refactoring types

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ import db from 'livedb'
 
 import client from './lib/client'
 import server from './lib/server'
-import types from './lib/types'
 import details from './package.json'
 
 export default {
     client,
     server,
-    types,
     db,
     version: details.version
 }
